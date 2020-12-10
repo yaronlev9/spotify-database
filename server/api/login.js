8496d78c9a9bd70c399763627607bda693fec9aa
yaronlev9 <62477626+yaronlev9@users.noreply.github.com>
2020-12-10 14:13:06 +0200

    fixed textual bugs

--- server/api/login.js
+++ server/api/login.js
@@ -1,31 +1,35 @@
 const { Router } = require('express');
 let jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 const router = Router();
+const saltRounds = 10;
 
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({
     where: {
-      username,
-      password
+      username: username,
     }
   });
-  if(!user) {
-    return res.status(500).json({
-      errorMessage: 'wrong login details'
-    })
-  }
-  let token = jwt.sign({username: username},
-    process.env.JWT_SECRET,
-    { expiresIn: '1h' // expires in 1 hours
+  bcrypt.compare(password, user.password, function(err, resp) {
+    if (resp){
+      let token = jwt.sign({username: username},
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' // expires in 1 hours
+        }
+      );
+        return res.json({
+        success: true,
+        token,
+      });
     }
-  );
-    return res.json({
-    success: true,
-    token,
+    else {
+      return res.status(500).json({
+        errorMessage: 'wrong login details'
+      })
+    }
   });
 });
 
